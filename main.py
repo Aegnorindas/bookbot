20b65a4c7c94318a33071e99bf50168186e5d3c2
Aegnorindas <aegnorindas@gmail.com>
2024-05-02 09:18:26 +0200

    Exercice 16

--- main.py
+++ main.py
@@ -13,18 +13,26 @@ def wordsCount(file_content: str):
 def charCount(file_content: str, char: str):
     return file_content.count(char)
 
+def printReport(words_count: int, char_count):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{words_count} words found in the document\n")
+    for cc in char_count:
+        print(f"The '{cc[0]}' character was found {cc[1]} times")
+
 def main() -> int:
     file = "books/frankenstein.txt"
-    char = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
+    char = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    char_count = {}
+    char_count = []
 
     file_content = getFileContent(file).lower()
     words_count = wordsCount(file_content)
-    for c in char:
-        char_count[c] = charCount(file_content, c)
 
-    print(char_count["r"])
+    for c in char:
+        char_count.append([c, charCount(file_content, c)])
+    char_count.sort(key=lambda char_count: char_count[1], reverse=True)
+
+    printReport(words_count, char_count)
 
     return 0
 
